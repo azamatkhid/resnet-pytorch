30911bff325514df2ae648b3a102c9d433e8ce87
azamat <@stunitas.com>
2020-01-31 17:33:59 +0900

    wip residual block implemented

--- network.py
+++ network.py
@@ -1,39 +1,80 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
+from torchsummary import summary
 
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1=nn.Conv2d(1,6,3)
-        self.conv2=nn.Conv2d(6,16,3)
-        self.fc1=nn.Linear(16*6*6,120)
-        self.fc2=nn.Linear(120,84)
-        self.fc3=nn.Linear(84,10)
+class ResBlock2(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size=3,padding=0,stride=1,activation=nn.ReLU):
+        super(ResBlock2,self).__init__()
+        self.res_block=nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding),
+                nn.BatchNorm2d(out_channels),
+                activation(inplace=True),
+                nn.Conv2d(out_channels,out_channels,kernel_size, stride=stride, padding=padding),
+                nn.BatchNorm2d(out_channels),
+                activation(inplace=True))
+        self.skip_conv=nn.Conv2d(in_channels,out_channels,1)
 
     def forward(self,x):
-        x=F.max_pool2d(F.relu(self.conv1(x)),(2,2))
-        x=F.max_pool2d(F.relu(self.conv2(x)),(2,2))
-        x=x.view(-1,self.num_flatten_features(x))
-        x=F.relu(self.fc1(x))
-        x=F.relu(self.fc2(x))
-        x=self.fc3(x)
+        residual=x
+        x=self.res_block(x)
+        if x.size()[1]!=residual.size()[1]:
+            residual=self.skip_conv(residual)
+        return x+residual
+
+class ResBlock3(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size=3,padding=0,stride=1,activation=nn.ReLU):
+        super(ResBlock3,self).__init__()
+        self.res_block=nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding),
+                nn.BatchNorm2d(out_channels),
+                activation(inplace=True),
+                nn.Conv2d(out_channels,out_channels,kernel_size, stride=stride, padding=padding),
+                nn.BatchNorm2d(out_channels),
+                activation(inplace=True),
+                nn.Conv2d(out_channels,out_channels,kernel_size, stride=stride, padding=padding),
+                nn.BatchNorm2d(out_channels),
+                activation(inplace=True))
+        self.skip_conv = nn.Conv2d(in_channels,out_channels,1)
+        
+    def forward(self,x):
+        residual=x
+        x=self.res_block(x)
+        if x.size()[1]!=residual.size()[1]:
+            residual=self.skip_conv(residual)
+        return x+residual
+
+class BasicBlock(nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0, pool=False):
+        super(BasicBlock,self).__init__()
+        self.conv=nn.Conv2d(in_channels,out_channels,kernel_size,stride=stride,padding=padding)
+        self.pool=pool
+        if pool:
+            self.pool=nn.MaxPool2d(kernel_size=2)
+    
+    def forward(self,x):
+        x=self.conv(x)
+        if self.pool:
+            return self.pool(x)
         return x
 
-    def num_flatten_features(self,x):
-        size=x.size()[1:]
-        num_features=1
-        for s in size:
-            num_features*=s
-        return num_features
+class ResNet(nn.Module):
+    def __init__(self, num_blocks=1):
+        super(ResNet,self).__init__()
+        blocks=[]
+        in_channels, out_channels=3,64
+        blocks.append(BasicBlock(in_channels,out_channels,7,pool=True))
+        in_channels=out_channels
+        for i in range(num_blocks):
+            blocks.append(ResBlock2(in_channels=in_channels,out_channels=out_channels,kernel_size=3,padding=1))
+
+        self.net=nn.Sequential(*blocks)
+
+    def forward(self,x):
+        return self.net(x)
 
 if __name__=="__main__":
-    net=CNN()
-    print(net)
-    weights=list(net.parameters())
-    rand_input=torch.rand(1,1,32,32)
-    out=net(rand_input)
-    print(out)
-    net.zero_grad()
-    out.backward(torch.randn(1,10))
-    print(out)
+    net=ResNet(num_blocks=2)
+    print(summary(net,(3,224,224)))
+
+    model=models.resnet18()
+    print(summary(model,(3,224,224)))
