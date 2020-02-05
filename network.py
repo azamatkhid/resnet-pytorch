3f9cdbd860b6c666a96e66688fca6d04e7984415
azamat <@stunitas.com>
2020-02-05 19:17:55 +0900

    wip bottleneck block

--- network.py
+++ network.py
@@ -24,8 +24,24 @@ class BasicBlock(nn.Module):
         out=out+self.skip(x)
         return out
 
+class Bottleneck(nn.Module):
+    def __init__(self,inchannel,outchannel,stride=1,kernel_size=3,act=nn.ReLU):
+        super(Bottleneck,self).__init__()
+
+
+
+
+
+
+
+
+
+
+
+
+
 class ResNet(nn.Module):
-    def __init__(self,layers=[2,2,2,2],act=nn.ReLU, num_classes=1000):
+    def __init__(self,layers=[2,2,2,2],act=nn.ReLU, num_classes=1000,res_block=BasicBlock):
         super(ResNet,self).__init__()
         inchannel,outchannel=3,64
         self._layers=[]
@@ -34,10 +50,10 @@ class ResNet(nn.Module):
         self.b0=nn.BatchNorm2d(outchannel)
         self.act0=act(inplace=True)
         self.maxpool=nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
-        self.layer1=self._make_res_layer(outchannel,outchannel,layers[0],stride=1,act=act)
-        self.layer2=self._make_res_layer(outchannel,2*outchannel,layers[1],stride=2,act=act)
-        self.layer3=self._make_res_layer(2*outchannel,4*outchannel,layers[2],stride=2,act=act)
-        self.layer4=self._make_res_layer(4*outchannel,8*outchannel,layers[3],stride=2,act=act)
+        self.layer1=self._make_res_layer(res_block,outchannel,outchannel,layers[0],stride=1,act=act)
+        self.layer2=self._make_res_layer(res_block,outchannel,2*outchannel,layers[1],stride=2,act=act)
+        self.layer3=self._make_res_layer(res_block,2*outchannel,4*outchannel,layers[2],stride=2,act=act)
+        self.layer4=self._make_res_layer(res_block,4*outchannel,8*outchannel,layers[3],stride=2,act=act)
         self.pooling=nn.AdaptiveAvgPool2d((1,1))
         self.layer5=nn.Linear(512, num_classes)
 
@@ -53,11 +69,11 @@ class ResNet(nn.Module):
         out=self.layer5(out)
         return out
 
-    def _make_res_layer(self,inchannel,outchannel,num_blocks,stride=1,act=nn.ReLU):
+    def _make_res_layer(self,res_block,inchannel,outchannel,num_blocks,stride=1,act=nn.ReLU):
         blocks=[]
-        blocks.append(BasicBlock(inchannel,outchannel,stride=stride,act=act))
+        blocks.append(res_block(inchannel,outchannel,stride=stride,act=act))
         for bl in range(1,num_blocks):
-            blocks.append(BasicBlock(outchannel,outchannel,stride=1,act=act))
+            blocks.append(res_block(outchannel,outchannel,stride=1,act=act))
         return nn.Sequential(*blocks)
 
 
